865a89dcf6efeb7b566bde83e5dfa51526e051e8
HellR@y <47401874+DanjokLion@users.noreply.github.com>
2024-01-23 16:14:18 +0300

    scale link & social

--- static/js/script.js
+++ static/js/script.js
@@ -2,3 +2,13 @@ function openMenu() {
     const nav = document.querySelector('nav');
     nav.classList.toggle('open');
 }
+
+document.querySelectorAll('.bx-icon, .link-site').forEach(item => {
+    item.addEventListener('mouseenter', () => {
+      item.style.transform = 'scale(1.2)';
+    });
+  
+    item.addEventListener('mouseleave', () => {
+      item.style.transform = 'scale(1)';
+    });
+  });
