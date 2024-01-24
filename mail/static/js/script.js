791aeb064b619b9ebca7a77e05ddc7295a97f7b2
HellR@y <47401874+DanjokLion@users.noreply.github.com>
2024-01-24 15:22:37 +0300

    validate, change

--- mail/static/js/script.js
+++ mail/static/js/script.js
@@ -10,7 +10,23 @@ function sendEmail() {
     const from = document.getElementById("from").value;
     const subject = document.getElementById("subject").value;
     const message = document.getElementById("message").value;
-
+    const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+    const input = document.querySelector('input');
+    
+    function isEmailValid(value) {
+         return EMAIL_REGEXP.test(value);
+    }
+    
+    function onInput() {
+        if (isEmailValid(input.value)) {
+            input.style.borderColor = 'green';
+        } else {
+            input.style.borderColor = 'red';
+        }
+    }
+    
+    input.addEventListener('input', onInput);
+    
     // Send the email using EmailJS
     emailjs.send("service_onhcdmr", "template_ixxo8he", {
         from_email: from,
